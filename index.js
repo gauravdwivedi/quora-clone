7de09b62b8acf9096f8b94299aa614b93851c4f6
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-09-06 18:32:18 +0530

    changed db

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ app.use(passport.setAuthenticatedUser);
 //routes
 app.use("/", require("./routes"));
 
-app.listen(port, (err) => {
+app.listen(process.env.PORT || 5000, (err) => {
   if (err) {
     console.log("Error in running the sever");
   }
