2b7a9b3b1c252dce2fcdba48f9508a16ef1b7370
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-09-06 17:08:30 +0530

    changed db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/quora', {
+const connection_url = 'mongodb+srv://whatsapp:VAJuVIsGrf60pBPX@cluster0.enhdn.mongodb.net/whatsappdb?retryWrites=true&w=majority';
+
+mongoose.connect(connection_url, {
+    useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -11,6 +14,7 @@ db.on('error', console.error.bind(console, 'Error connecting to mongodb'));
 
 db.once('open', () => {
     console.log('Connected successfully to Mongodb');
+    
 });
 
 module.exports = db;
